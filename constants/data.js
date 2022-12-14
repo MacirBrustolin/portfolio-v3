d509a5c68843808d0a3d49782c0a6d519084fb5b
Macir Brustolin <mj.brustolin@hotmail.com>
2022-12-14 14:44:23 -0300

    first commit

---
+++ constants/data.js
@@ -0,0 +1,45 @@
+const userData = {
+  githubUsername: "macirbrustolin",
+  name: "Macir Brustolin",
+  designation: "Full-Stack Web Developer",
+  avatarUrl: "/avatar.png",
+  email: "mj.brustolin@hotmail.com",
+  phone: "(45) 9 9984-9698",
+  address: "Cascavel - Paraná - Brasil",
+  projects: [
+    {
+      title: "Tailwind Master Kit",
+      link: "https://tailwindmasterkit.com",
+      imgUrl: "/tmk.jpg",
+    }
+  ],
+  about: {
+    title:
+      "Pequena descrição sobre mim.",
+    description: [
+      `Descrição mais detalhada sobre mim.`,
+    ],
+    currentProject: "Aplicação WEB Full-Stack com Next,js e Firebase.",
+    currentProjectUrl: "",
+  },
+  experience: [
+    {
+      title: "Graduação",
+      company: "UTFPR - Medianeira",
+      year: "2020",
+      companyLink: "http://www.utfpr.edu.br/campus/medianeira",
+      desc: "Formação acadêmica em Engenharia Elétrica",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1xmE3BOmgM7TAOOgVp36xQIQvYDntDYoo/view?usp=sharing",
+  socialLinks: {
+    instagram: "https://www.instagram.com/macirjr",
+    twitter: "https://twitter.com/Macirjunior",
+    linkedin: "https://www.linkedin.com/in/macirbrustolin/",
+    github: "https://github.com/MacirBrustolin",
+    facebook: "https://www.facebook.com/macir.brustolin/",
+  },
+};
+
+export default userData;
